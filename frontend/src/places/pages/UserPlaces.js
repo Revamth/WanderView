5656d2e75d4aa2a01576d620a096c7f6f3f7302c
Revamth <rvrevamth@gmail.com>
2025-05-27 19:40:16 +0530

    Created Places components and basic interface for geolocation

---
+++ frontend/src/places/pages/UserPlaces.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Emperor State Building",
+    description: "One of the most famous Sky Scrapers in thw world",
+    imageUrl:
+      "https://assets.simpleviewinc.com/simpleview/image/upload/q_75/v1/crm/newyorkstate/GettyImages-486334510_CC36FC20-0DCE-7408-77C72CD93ED4A476-cc36f9e70fc9b45_cc36fc73-07dd-b6b3-09b619cd4694393e.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      long: -73.9882393,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emperor State Building",
+    description: "One of the most famous Sky Scrapers in thw world",
+    imageUrl:
+      "https://assets.simpleviewinc.com/simpleview/image/upload/q_75/v1/crm/newyorkstate/GettyImages-486334510_CC36FC20-0DCE-7408-77C72CD93ED4A476-cc36f9e70fc9b45_cc36fc73-07dd-b6b3-09b619cd4694393e.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      long: -73.9882393,
+    },
+    creator: "u2",
+  },
+];
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
