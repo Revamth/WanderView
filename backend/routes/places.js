1d7526ddef05f7e6b9a1c86d8f54d8f568c5721e
Revamth <rvrevamth@gmail.com>
2025-06-04 16:31:32 +0530

    Started working on Backend

---
+++ backend/routes/places.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const placesControllers = require("../controllers/places");
+
+const router = express.Router();
+
+router.get("/:pid", placesControllers.getPlaceById);
+
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
+
+module.exports = router;
